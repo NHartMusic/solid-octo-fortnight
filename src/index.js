cfd68d22cce2d29c97c0bfab0e2030e59169628a
nick_hart <nhartmusic@gmail.com>
2023-11-10 14:15:45 -0800

    successfully reading data from firebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+import { initializeApp } from 'firebase/app'
+import {
+    collection,
+    getDocs,
+    getFirestore
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBV7Oyfx4ZgO2fLk8E9SniNARzEVCUfDW4",
+    authDomain: "end-occupation-now.firebaseapp.com",
+    projectId: "end-occupation-now",
+    storageBucket: "end-occupation-now.appspot.com",
+    messagingSenderId: "197992597500",
+    appId: "1:197992597500:web:23773fb981dae416ad5479",
+    measurementId: "G-EZ2BNWVEFW"
+  };
+
+//init app 
+initializeApp(firebaseConfig)
+
+//init services
+const db = getFirestore()
+
+//collection reference
+const colRef = collection(db, 'Total_Spending_2022')
+
+//get collection data 
+getDocs(colRef)
+  .then((snapshot) => {
+      let countries = []
+      snapshot.docs.forEach((doc) => {
+        countries.push({ ...doc.data(), id: doc.id })
+      })
+      console.log(countries)
+  })
+
 
 const addCountryForm = document.querySelector('.add')
 addCountryForm.addEventListener('submit', (e) => {
